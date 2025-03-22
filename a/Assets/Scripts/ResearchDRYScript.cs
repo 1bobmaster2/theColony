9966d1874bfc24f7f053b56eb8c1cc5bfaf0eaa2
1bobmaster2 <fkuba556@gmail.com>
2025-03-22 19:28:46 +0100

    working on it more

--- a/Assets/Scripts/ResearchDRYScript.cs
+++ a/Assets/Scripts/ResearchDRYScript.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ResearchDRYScript : MonoBehaviour
@@ -5,18 +7,38 @@ public class ResearchDRYScript : MonoBehaviour
     [SerializeField] private int researchCost;
     private Stats stats;
     private GameObject statsObject;
+    private bool isResearched;
+    public List<ResearchDRYScript> requiredResearch = new List<ResearchDRYScript>();
+    private bool canBeResearched = true;
 
     void Start()
     {
         statsObject = GameObject.FindWithTag("statsManager");
         stats = statsObject.GetComponent<Stats>();
+        
     }
     
     public void AssingResearchPoints()
     {
-        if (stats.researchPointsInStock >= researchCost )
+        canBeResearched = true;
+        
+        foreach (ResearchDRYScript r in requiredResearch)
+        {
+            if (r.isResearched == true)
+            {
+                continue;
+            }
+            else
+            {
+                canBeResearched = false;
+                Debug.Log("not everything required was researched");
+            }
+        }
+        
+        if (stats.researchPointsInStock >= researchCost && !isResearched && canBeResearched)
         {
             stats.researchPointsInStock -= researchCost;
+            isResearched = true;
             Debug.Log("Researched");
         }
         else
