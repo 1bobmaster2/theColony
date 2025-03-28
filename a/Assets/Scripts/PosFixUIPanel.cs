6009ff2997ce596bcac91eb55ba626ca73352049
1bobmaster2 <fkuba556@gmail.com>
2025-03-28 20:31:36 +0100

    fixed positioning issue

--- a/Assets/Scripts/PosFixUIPanel.cs
+++ a/Assets/Scripts/PosFixUIPanel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PosFixUIPanel : MonoBehaviour
@@ -5,11 +6,26 @@ public class PosFixUIPanel : MonoBehaviour
     public RectTransform desiredPosition; // the desired position
     public Vector2 desiredPositionVector; // the desired position, but in vector 2 variable
 
-
-    // Update is called once per frame
+    public bool isGroup;                                      //
+    public List<GameObject> group = new List<GameObject>();
     void Update()
     {
-        // set the desire position right
-        desiredPosition.anchoredPosition = desiredPositionVector;
+        if (!isGroup)
+        {
+            // set the desire position right
+            desiredPosition.anchoredPosition = desiredPositionVector;
+        }
+    }
+
+    void OnEnable()
+    {
+        if (isGroup)
+        {
+            foreach (GameObject obj in group)
+            {
+                RectTransform rectTransform = obj.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2(0, 0);
+            }
+        }
     }
 }
