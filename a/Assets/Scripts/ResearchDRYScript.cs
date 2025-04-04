43486b29fa66b353c976b851b9e21770c6a92a5a
1bobmaster2 <fkuba556@gmail.com>
2025-04-04 19:41:56 +0200

    about to test right clikc
    only commiting it here to avoid future errors

--- a/Assets/Scripts/ResearchDRYScript.cs
+++ a/Assets/Scripts/ResearchDRYScript.cs
@@ -46,44 +46,16 @@ public class ResearchDRYScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1)) 
-        {
-            Debug.Log("clicked");
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
-            Debug.Log(hitCollider);
+        
+        
+    }
 
-           
-            PointerEventData pointerData = new PointerEventData(EventSystem.current)
-            {
-                position = Input.mousePosition
-            };
-
-            
-            RaycastResult uiRaycastResult = new RaycastResult();
-            GraphicRaycaster graphicRaycaster = canvasTransform.GetComponent<GraphicRaycaster>();
-            if (graphicRaycaster != null)
-            {
-                graphicRaycaster.Raycast(pointerData, new System.Collections.Generic.List<RaycastResult>());
-                if (uiRaycastResult.gameObject != null) 
-                {
-                    Debug.Log("UI object clicked");
-                    return; 
-                }
-            }
-
-            
-            if (hitCollider != null && hitCollider.gameObject == gameObject)
-            {
-                GameObject obj = hitCollider.gameObject;
+    
+    /*GameObject obj = hitCollider.gameObject;
                 ResearchDRYScript researchScript = obj.GetComponent<ResearchDRYScript>();
                 GameObject go = Instantiate(researchWindowObject, obj.transform.position, Quaternion.identity, canvasTransform);
                 SeResearchData seResearchData = go.GetComponent<SeResearchData>();
-                seResearchData.researchPoints = researchCost;
-            }
-        }
-    }
-    
+                seResearchData.researchPoints = researchCost;*/
 
     public void AssingResearchPoints()
     {
