558a3702a191da7ea77750023c0e18b2a919242d
1bobmaster2 <fkuba556@gmail.com>
2025-04-08 15:56:44 +0200

    added a separate options menu
    hi

--- a/Assets/Scripts/UI.cs
+++ a/Assets/Scripts/UI.cs
@@ -4,11 +4,13 @@ public class UI : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject controls;
+    [SerializeField] private GameObject options;
 
     public void EnableControls()
     {
         controls.SetActive(true);
         menu.SetActive(false);
+        options.SetActive(false);
     }
 
     void DisableControls()
@@ -16,15 +18,35 @@ public class UI : MonoBehaviour
         if (controls.activeInHierarchy)
         {
             controls.SetActive(false);
-            menu.SetActive(true);
+            menu.SetActive(false);
+            options.SetActive(true);
         }
     }
+
+    public void EnableOptions()
+    {
+        options.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void DisableOptions()
+    {
+        options.SetActive(false);
+        menu.SetActive(true);
+    }
+    
+    
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             DisableControls();
+        }
+
+        if (options.activeInHierarchy)
+        {
+            menu.SetActive(false);
         }
     }
 }
