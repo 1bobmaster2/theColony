ba33e8d5f00ac6d905881e2ada36cee52e1e9dea
1bobmaster2 <fkuba556@gmail.com>
2025-04-23 19:36:42 +0200

    removed unity's splash screen

---
+++ a/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialWindow;
+    [SerializeField] private Transform tutorialUIObject;
+    [Space]
+    [SerializeField] private string tutorialText;
+
+    private bool anyMoreTutorial;
+    void Start()
+    {
+        
+    }
+
+    void StartTutorialing()
+    {
+        
+    }
+}
