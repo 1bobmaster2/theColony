16311196876ce12c6210c4eeb38b1d8968115243
1bobmaster2 <fkuba556@gmail.com>
2025-04-24 18:50:09 +0200

    position sets correctly

--- a/Assets/Scripts/Tutorial.cs
+++ a/Assets/Scripts/Tutorial.cs
@@ -6,16 +6,17 @@ public class Tutorial : MonoBehaviour
     [SerializeField] private Transform tutorialUIParentObject;
     [Space]
     [SerializeField] private string tutorialText;
-    private Vector3 tutorialPosition = new Vector3(409, 384, 0);
+    private Vector3 tutorialPosition = new(537, 384, 0);
     private Quaternion tutorialRotation = Quaternion.identity;
     public static bool anyMoreTutorial;
     void Start()
     {
-        StartTutorialing();
+        Invoke(nameof(StartTutorialing), 5f);
     }
 
     void StartTutorialing()
     {
-        Instantiate(tutorialWindow, tutorialPosition, tutorialRotation, tutorialUIParentObject);
+        GameObject tutorialWindowInstantiated = Instantiate(tutorialWindow, tutorialPosition, tutorialRotation, tutorialUIParentObject);
+        tutorialWindowInstantiated.GetComponent<RectTransform>().anchoredPosition = tutorialPosition;
     }
 }
